82518e1ad3c9b366a428d845e8c693def13d4767
nerdyLawman <andrewtlyman@gmail.com>
2014-03-11 18:57:49 -0600

    changed STATIC_ROOT to STATIC_URL in bgSelect

--- openspace/libs/siteHelpers.py
+++ openspace/libs/siteHelpers.py
@@ -6,7 +6,7 @@ from random import randint
 from os.path import join
 from sys import path
 import os, fnmatch, re
-from settings.common import STATIC_ROOT
+from settings.common import STATIC_URL
 
 def fairPlay(request):
     """Returns True if current session_profile is active and page should drain energy."""
@@ -112,7 +112,7 @@ def addTags(obj, tags):
 
 def bannerSelect(current):
     """Returns a randomly selected banner image from collection of banners. Used in bruce_banner block."""
-    dirpath = join(STATIC_ROOT, 'assets/media/banners/')
+    dirpath = join(STATIC_URL, 'media/banners/')
     top = len(fnmatch.filter(os.listdir(dirpath), '*jpg'))
     selection = randint(1, top)
     while selection == current:
@@ -121,7 +121,7 @@ def bannerSelect(current):
 
 def bgSelect(current):
     """Returns next background image from collection of backgrounds. Used in background-style.html."""
-    dirpath = join(STATIC_ROOT, '/media/backgrounds/')
+    dirpath = join(STATIC_URL, 'media/backgrounds/')
     if current > len(fnmatch.filter(os.listdir(dirpath), '*jpg')):
         return randint(1, len(fnmatch.filter(os.listdir(dirpath), '*jpg')))
     selection = current + 1
